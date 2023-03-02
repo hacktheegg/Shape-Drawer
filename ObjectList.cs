946fa6f83dee91d6638b81f8b9f99dd6a032b047
hacktheegg <jarrahvanritten@gmail.com>
2023-03-03 00:09:59 +1100

    Add project files.

---
+++ ObjectList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_Drawer
+{
+    class Square
+    {
+        public int width;
+        public int height;
+        public Tuple<int, int> originPoint;
+
+        public Square()
+        {
+            width = 0;
+            height = 0;
+            originPoint = Tuple.Create(0, 0);
+        }
+    }
+    class Circle
+    {
+        int radius = 0;
+        public Tuple<int, int> originPoint;
+        public Circle()
+        {
+            radius = 0;
+            originPoint = Tuple.Create(0, 0);
+        }
+
+    }
+    class Triangle
+    {
+        public Tuple<int, int> pointTop;
+        public Tuple<int, int> pointTwo;
+        public Tuple<int, int> originPoint;
+        public Triangle()
+        {
+            pointTop = Tuple.Create(0, 0);
+            pointTwo = Tuple.Create(0, 0);
+            originPoint = Tuple.Create(0, 0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Shape_Drawer;
+
+class Program
+{
+    
+    public static Array[] Board = Array.Empty<Array>();
+    
+    static void Main(string[] args)
+    {
+        int tempInt;
+
+        FailedIntInput:
+            Console.Write("Size of board (square): ");
+            string SizeOfBoardInput = Console.ReadLine();
+
+        if (!int.TryParse(SizeOfBoardInput, out tempInt))
+        {
+            goto FailedIntInput;
+        }
+
+        
+
+
+
+
+        while (true)
+        {
+
+        }
+    }
+}
+
+
+
+//Square mySquare = new Square(5,8,Tuple.Create<int,int> (1,1));
+/*public Square(int x, int y, Tuple<int,int> mytup)
+{
+    width = x;
+    height = y;
+    originPoint = mytup;
+}*/
